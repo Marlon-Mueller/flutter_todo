aa6dd9de120d2e64c50cdb3df32a4fdbe75dc180
Marlon-Mueller <76700334+Marlon-Mueller@users.noreply.github.com>
2021-07-03 15:49:50 +0200

    all card examples

--- lib/main.dart
+++ lib/main.dart
@@ -21,15 +21,35 @@ void main() async {
   await Hive.openBox<ToDoList>('Freetime');
   await Hive.openBox<ToDoList>('Household'); */
   var todo = ToDoList(name: 'Mathe', tasks: []);
-
   var todo2 = ToDoList(name: 'Physik', tasks: []);
-
   var todo3 = ToDoList(name: 'Deutsch', tasks: []);
+  
+  var todo01 = ToDoList(name: 'Termine', tasks: []);
+  var todo21 = ToDoList(name: 'Arbeit', tasks: []);
+  
+  var todo02 = ToDoList(name: 'Sport', tasks: []);
+  var todo22 = ToDoList(name: 'Spaß', tasks: []);
+  var todo32 = ToDoList(name: 'Essen', tasks: []);
+  
+  var todo03 = ToDoList(name: 'Kochen', tasks: []);
+  var todo23 = ToDoList(name: 'Waschen', tasks: []);
+  var todo33 = ToDoList(name: 'Garten', tasks: []);
 
   await HiveService().saveList('Schule', todo);
   await HiveService().saveList('Schule', todo2);
   await HiveService().saveList('Schule', todo3);
-
+  
+  await HiveService().saveList('Arbeit', todo01);
+  await HiveService().saveList('Arbeit', todo21);
+  
+  await HiveService().saveList('Freizeit', todo02);
+  await HiveService().saveList('Freizeit', todo22);
+  await HiveService().saveList('Freizeit', todo32);
+  
+  
+  await HiveService().saveList('Haushalt', todo03);
+  await HiveService().saveList('Haushalt', todo23);
+  await HiveService().saveList('Haushalt', todo33);
   runApp(MyApp());
 }
 
